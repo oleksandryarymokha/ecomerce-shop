7a14d4d5e3697c3ef42ba38967585bf4e9cfbf79
alex <oleksandr.yarymokha@gmail.com>
2026-07-17 17:42:07 +0200

    Fix entity: cart.entity.ts

--- src/carts/cart.entity.ts
+++ src/carts/cart.entity.ts
@@ -1,8 +1,9 @@
 import { Position } from '../positions/position.entity';
+import { Customer } from '../customers/customer.entity';
 
 export class Cart {
   id: number;
-  customerId: number;
+  customer: Customer;
   products: Position[];
   totalPrice: number;
 }
