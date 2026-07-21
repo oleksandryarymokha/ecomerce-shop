4188af158978b43c9fb40bb51e308ca8c597f5d1
alex <oleksandr.yarymokha@gmail.com>
2026-07-21 15:42:32 +0200

    Add database connection string to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,27 @@ import { ProductsModule } from './products/products.module';
 import { CustomerModule } from './customers/customers.module';
 import { PositionModule } from './positions/positions.module';
 import { CartModule } from './carts/carts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// postgresql://postgres:qwerty123@localhost:5432
 
 @Module({
-  imports: [ProductsModule, CustomerModule, PositionModule, CartModule],
+  imports: [
+    ProductsModule,
+    CustomerModule,
+    PositionModule,
+    CartModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'qwerty123',
+      database: 'mydb',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
