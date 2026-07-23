5c6809a31966f173b2084af8d761e9b3e79e4e06
Christina <khryschristinaglinkina90@gmail.com>
2026-07-23 12:52:54 +0200

    feat: add TypeORM config and connect CustomerModule

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CustomerController } from './customers.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Customer } from './customer.entity';
 
 @Module({
   controllers: [CustomerController],
+  imports: [TypeOrmModule.forFeature([Customer])],
 })
 export class CustomerModule {}
